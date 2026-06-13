b24f6f9d209e876e42df50b073f72afdfaae5b35
suryaraja3 <124482043+suryaraja3@users.noreply.github.com>
2026-06-13 15:43:35 +0530

    added a putmapping and learned about RequestMapping

--- ecom-application/ecom-application/src/main/java/com/app/ecom/UserController.java
+++ ecom-application/ecom-application/src/main/java/com/app/ecom/UserController.java
@@ -11,27 +11,31 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
+// this another way of using requestmapping at the class level and it is mostly used versioning of api or the repeated part of the api .
 public class UserController {
 
-    private final  UserService userService;
+    private final UserService userService;
 
 //    public UserController(UserService userList) {
 //        this.userList = userList;
 //    }
 
-    @GetMapping("/api/users")
+    @GetMapping
+//    @RequestMapping(value = "/api/users",method = RequestMethod.GET)// this is the just another way of
+    // using getmapiing is the just the internal implementation of the getmapping nad this one way of  using requestmaping at method level.
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.FetchAllUser(), HttpStatus.OK);
 //        return  ResponseEntity.ok(userService.FetchAllUser());
     }
 
-    @PostMapping("/api/users")
+    @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
         return ResponseEntity.ok("User added successfully");
     }
 
-    @GetMapping("/api/users/{id}")
+    @GetMapping("{id}")
     public ResponseEntity<User> getAllUser(@PathVariable Long id) {
 //        User user = userService.fetchUser(id);
 //        if (user == null){
@@ -41,7 +45,17 @@ public class UserController {
 
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
-                .orElseGet(()->ResponseEntity.notFound().build());
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<String> createUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        boolean updated = userService.updateUser(id, updatedUser);
+        if (updated) {
+            return ResponseEntity.ok("User updated successfully");
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 
 }
