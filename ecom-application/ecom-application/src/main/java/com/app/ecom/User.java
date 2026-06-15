fea10e3ccd66ab1a102a1f22973577db8f5c2099
suryaraja3 <124482043+suryaraja3@users.noreply.github.com>
2026-06-15 14:56:08 +0530

    Added H2 database learned about JPA and changed all the LIST based impl in the UserService in to JPA impl

--- ecom-application/ecom-application/src/main/java/com/app/ecom/User.java
+++ ecom-application/ecom-application/src/main/java/com/app/ecom/User.java
@@ -1,9 +1,17 @@
 package com.app.ecom;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "user_table")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
--- ecom-application/ecom-application/src/main/java/com/app/ecom/UserService.java
+++ ecom-application/ecom-application/src/main/java/com/app/ecom/UserService.java
@@ -1,21 +1,33 @@
 package com.app.ecom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    private List<User> userList = new ArrayList<>();
+
+    private final UserRepository userRepository;
+//    public UserService(UserrRepository userRepository) { instead of this we can add required args constructor
+//        this.userRepository = userRepository;
+//    }
+
+    //private List<User> userList = new ArrayList<>();
     private Long userCount = 1L;
+
+
+
     public List<User> FetchAllUser(){
-        return userList;
+//        return userList;
+        return userRepository.findAll();
     }
     public void addUser( User user) {
-        user.setId(userCount++);
-        userList.add(user);
+//        user.setId(userCount++);
+//        userList.add(user);
+        userRepository.save(user);
     }
 
     public Optional<User> fetchUser(Long id) {
@@ -25,19 +37,27 @@ public class UserService {
 //            }
 //        }
 //        return  null;
-        return userList.stream()
-                .filter(user-> user.getId().equals(id))
-                .findFirst();
+//        return userList.stream()
+//                .filter(user-> user.getId().equals(id))
+//                .findFirst();
+        return userRepository.findById(id);
     }
 
 
     public  boolean updateUser(Long id, User updateUser){
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst()
-                .map(existingUser ->  {
+//        return userList.stream()
+//                .filter(user -> user.getId().equals(id))
+//                .findFirst()
+//                .map(existingUser ->  {
+//                    existingUser.setFirstName(updateUser.getFirstName());
+//                    existingUser.setLastName(updateUser.getLastName());
+//                    return  true;
+//                }).orElse(false);
+        return userRepository.findById(id)
+                 .map(existingUser ->  {
                     existingUser.setFirstName(updateUser.getFirstName());
                     existingUser.setLastName(updateUser.getLastName());
+                    userRepository.save(existingUser);
                     return  true;
                 }).orElse(false);
 
